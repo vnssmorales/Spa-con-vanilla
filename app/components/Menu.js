71a4982a3ef24b2e8330ad27a00ead82cf9e9317
vanemh <vnss.morales@gmail.com>
2023-05-10 13:55:49 -0400

    creo header y componentes para el header

---
+++ app/components/Menu.js
@@ -0,0 +1,11 @@
+export function Menu() {
+  const $menu = document.createElement("nav");
+  $menu.classList.add("menu");
+  $menu.innerHTML = `
+    <a href="#/" >Home</a>
+    <span> - </span>
+    <a href="#/search" >Búsqueda</a>
+    `;
+
+  return $menu;
+}
---
+++ app/components/SearchForm.js
@@ -0,0 +1,11 @@
+export function SearchForm() {
+    const $form = document.createElement("form"),
+    $input = document.createElement("input");
+
+    $form.classList.add("form-search");
+    $input.type = "search";
+    $input.placeholder = "Buscar..."
+
+    $form.appendChild($input);
+    return  $form;
+}
