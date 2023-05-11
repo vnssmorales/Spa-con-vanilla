3bea65800eae0bdb860061181559f22fa1d74343
vanemh <vnss.morales@gmail.com>
2023-05-11 12:55:16 -0400

    refactorizando el codigo

--- app/components/Router.js
+++ app/components/Router.js
@@ -1,30 +1,33 @@
 import api from "../helpers/wp_api.js";
-import {ajax} from "../helpers/ajax.js";
+import { ajax } from "../helpers/ajax.js";
 import { PostCard } from "./PostCard.js";
 
-export function Router() {
-    //objeto location trae internamente la propiedad de la url
-    let {hash} = location;
-     console.log(hash);
+export async function Router() {
+  const $main = document.getElementById("main");
+  //objeto location trae internamente la propiedad de la url
+  let { hash } = location;
+  console.log(hash);
 
-if(!hash || hash === "#/"){
-    document.getElementById("posts").innerHTML = "<h2>Sección del home</h2>";
-}else if(hash.includes("#/search")) {
-    document.getElementById("posts").innerHTML = "<h2>Sección del buscador</h2>";
-}else if(hash === "#/contacto"){
-    document.getElementById("posts").innerHTML = "<h2>Sección del contacto</h2>"
-}else{
-    document.getElementById("posts").innerHTML = "<h2>Aqui cargara el contenido del post previamente seleccionado</h2>"
-}
+  $main.innerHTML = null; //para asegurarnos que el contenido previo no este cargado
 
-    ajax({
-        url: api.POSTS,
-        cbSuccess: (posts) => {
-          console.log(posts);
-          let html = "";
-          posts.forEach((post) => html += PostCard(post));
-          document.querySelector(".loader").style.display = "none";
-          document.getElementById("posts").innerHTML = html;
-        }
-      })
+  if (!hash || hash === "#/") {
+    await ajax({
+      url: api.POSTS,
+      cbSuccess: (main) => {
+        console.log(main);
+        let html = "";
+        main.forEach((main) => (html += PostCard(main)));
+        document.querySelector(".loader").style.display = "none";
+        $main.innerHTML = html;
+      },
+    });
+  } else if (hash.includes("#/search")) {
+    $main.innerHTML = "<h2>Sección del buscador</h2>";
+  } else if (hash === "#/contacto") {
+    $main.innerHTML = "<h2>Sección del contacto</h2>";
+  } else {
+    $main.innerHTML =
+      "<h2>Aqui cargara el contenido del post previamente seleccionado</h2>";
+  }
+  document.querySelector(".loader").style.display = "none";
 }
